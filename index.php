771b502b6ea415e634de0fc1cb20ee5f83f39258
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-12-18 19:01:34 +0100

    end

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first php </title>
+</head>
+<body>
+    <?php $myVariable = "Hello World!" ?>
+    <h1><?php echo $myVariable?></h1>
+</body>
+</html>
